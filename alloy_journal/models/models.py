06ca72230266b63032442b3d1a7740bc595ed4da
telenocodoo <45532030+telenocodoo@users.noreply.github.com>
2019-11-13 18:19:25 +0500

    Add files via upload

---
+++ alloy_journal/models/models.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.move"
+
+    employee=fields.Many2one("hr.employee","Employee",compute="employee_hournal_view")
+    # employee_name=fields.Char(related='employee.name')
+
+    @api.one
+    def employee_hournal_view(self):
+        self.ensure_one()
+        self.employee=self.line_ids[0].x_studio_field_n0ak7
+
+
+#class AccountMoveLine(models.Model):
+ #   _inherit = "account.move.line"
+
+  #  employee = fields.Many2one("hr.employee", "Employee")
+
+
+
+
+
