0424f251a231194a9c8ff5212465c31e332b3901
telenocodoo <45532030+telenocodoo@users.noreply.github.com>
2019-11-13 18:38:25 +0500

    Update models.py

--- alloy_journal/models/models.py
+++ alloy_journal/models/models.py
@@ -7,12 +7,13 @@ class AccountInvoice(models.Model):
     _inherit = "account.move"
 
     employee=fields.Many2one("hr.employee","Employee",compute="employee_hournal_view")
-    # employee_name=fields.Char(related='employee.name')
+    employee_name=fields.Char(related='employee.name')
 
     @api.one
     def employee_hournal_view(self):
         self.ensure_one()
         self.employee=self.line_ids[0].x_studio_field_n0ak7
+        self.employee_name=self.employee.name
 
 
 #class AccountMoveLine(models.Model):
